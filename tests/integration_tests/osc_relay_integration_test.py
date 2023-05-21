9d78b0d58c62b153ceb812fa964edd51d405e3a2
DESKTOP2022\h195m <h195mic@gmail.com>
2023-05-21 22:51:07 +0900

    flake8

--- tests/integration_tests/osc_relay_integration_test.py
+++ tests/integration_tests/osc_relay_integration_test.py
@@ -1,22 +1,18 @@
 import socket
-import pytest
 from threading import Thread
 from time import perf_counter, sleep
 
 from pythonosc import dispatcher, osc_server, udp_client
 
 from oscduplicator.duplicator import OSCDuplicator
-from oscduplicator.setting import TransmitPortSetting
 
 
 class OscRelayIntegrationTest:
-
-
     def __init__(self) -> None:
-         self.result_9003 = []
-         self.result_9004 = []
-         self.result_9005 = []
-         self.result_9006 = []
+        self.result_9003 = []
+        self.result_9004 = []
+        self.result_9005 = []
+        self.result_9006 = []
 
     def run_duplicator(self):
         duplicator = OSCDuplicator()
@@ -25,7 +21,11 @@ class OscRelayIntegrationTest:
 
         duplicator.load_settings(SAVE_FILE)
 
-        th = Thread(target=duplicator.transmit_msg, args=(duplicator.clients, ), daemon=True)
+        th = Thread(
+            target=duplicator.transmit_msg,
+            args=(duplicator.clients,),
+            daemon=True,
+        )
         th.start()
 
         duplicator.start_server()
@@ -37,11 +37,11 @@ class OscRelayIntegrationTest:
         """
 
         def __client(port: int) -> udp_client.SimpleUDPClient:
-                hostname = socket.gethostname()
-                ip = socket.gethostbyname(hostname)
-                str_ip = str(ip)
-                return udp_client.SimpleUDPClient(str_ip, port)
-        
+            hostname = socket.gethostname()
+            ip = socket.gethostbyname(hostname)
+            str_ip = str(ip)
+            return udp_client.SimpleUDPClient(str_ip, port)
+
         client = __client(9000)
 
         count = 0
@@ -55,7 +55,7 @@ class OscRelayIntegrationTest:
         """
         OSCの受信サーバーを立ち上げ
         """
-        
+
         def __launch_osc_receiver(port: int):
             dpt = dispatcher.Dispatcher()
             dpt.map("/*", self.osc_handler, port)
@@ -64,7 +64,10 @@ class OscRelayIntegrationTest:
             server.serve_forever()
 
         ports = [9003, 9004, 9005, 9006]
-        receivers_thread = [Thread(target=__launch_osc_receiver, args=(port, )) for port in ports]
+        receivers_thread = [
+            Thread(target=__launch_osc_receiver, args=(port,))
+            for port in ports
+        ]
 
         for th in receivers_thread:
             th.start()
@@ -78,6 +81,7 @@ class OscRelayIntegrationTest:
             self.result_9005.append(args[0])
         elif port[0] == 9006:
             self.result_9006.append(args[0])
+
 
 def check_receiverd_value(result_: list) -> bool:
     """
@@ -95,13 +99,13 @@ def check_receiverd_value(result_: list) -> bool:
         return False
 
     for i in range(len(result_) - 1):
-        if result_[i] >= result_[i+1]:
+        if result_[i] >= result_[i + 1]:
             return False
-    
+
     return True
 
-def test_osc_relay_integration():
 
+def test_osc_relay_integration():
     test = OscRelayIntegrationTest()
 
     th0 = Thread(target=test.run_duplicator, daemon=True)
@@ -117,8 +121,7 @@ def test_osc_relay_integration():
 
     sleep(3)
 
-    assert check_receiverd_value(test.result_9003) == True
-    assert check_receiverd_value(test.result_9004) == True
-    assert check_receiverd_value(test.result_9005) == True
+    assert check_receiverd_value(test.result_9003) is True
+    assert check_receiverd_value(test.result_9004) is True
+    assert check_receiverd_value(test.result_9005) is True
     assert len(test.result_9006) == 0
-
--- tests/test_duplicator.py
+++ tests/test_duplicator.py
@@ -1,8 +1,8 @@
-import random, string
+import random
+import string
 from threading import Thread
 from time import sleep
 
-import pytest
 from pythonosc import osc_server
 
 from oscduplicator.duplicator import OSCDuplicator
@@ -39,11 +39,15 @@ class TestDuplicator:
 
         sleep(1)
         assert (
-            __server_is_shut_down(self.osc_duplicator._OSCDuplicator__server) is False
+            __server_is_shut_down(self.osc_duplicator._OSCDuplicator__server)
+            is False
         )
         self.osc_duplicator.stop_server()
         sleep(1)
-        assert __server_is_shut_down(self.osc_duplicator._OSCDuplicator__server) is True
+        assert (
+            __server_is_shut_down(self.osc_duplicator._OSCDuplicator__server)
+            is True
+        )
 
     def __transmit_port_settings(self):
         """
@@ -64,66 +68,98 @@ class TestDuplicator:
 
         assert self.__transmit_port_settings()[0].name == "test0_t"
         assert self.__transmit_port_settings()[0].port == 9001
-        assert self.__transmit_port_settings()[0].enabled == True
+        assert self.__transmit_port_settings()[0].enabled is True
 
         assert self.__transmit_port_settings()[1].name == "test1_t"
         assert self.__transmit_port_settings()[1].port == 9002
-        assert self.__transmit_port_settings()[1].enabled == True
+        assert self.__transmit_port_settings()[1].enabled is True
 
         assert self.__transmit_port_settings()[4].name == "test4_f"
         assert self.__transmit_port_settings()[4].port == 9004
-        assert self.__transmit_port_settings()[4].enabled == False
+        assert self.__transmit_port_settings()[4].enabled is False
 
         print(len(self.osc_duplicator.clients))
 
-        assert set(client._port for client in self.osc_duplicator.clients) == set([9001, 9002, 9003])
+        assert set(
+            client._port for client in self.osc_duplicator.clients
+        ) == set([9001, 9002, 9003])
 
     def test_update_settings(self):
         """
         class Duplicatorの設定を書き換えれるかテスト
         """
+
         def __random_name():
-            return "".join(random.choices(string.ascii_letters + string.digits, k=8))
+            return "".join(
+                random.choices(string.ascii_letters + string.digits, k=8)
+            )
 
         def __random_port():
             return random.randrange(start=0, stop=65535, step=1)
 
         # Arrange
         new_receive_port = 8001
-        new_transmit_port_settings = [TransmitPortSetting(__random_name(), __random_port(), True) for _ in range(4)]
+        new_transmit_port_settings = [
+            TransmitPortSetting(__random_name(), __random_port(), True)
+            for _ in range(4)
+        ]
 
         # Act
-        self.osc_duplicator.update_settings(new_receive_port, new_transmit_port_settings)
+        self.osc_duplicator.update_settings(
+            new_receive_port, new_transmit_port_settings
+        )
 
         # Assert
         assert self.osc_duplicator.receive_port == 8001
 
-        assert self.__transmit_port_settings()[0].name == new_transmit_port_settings[0].name
-        assert self.__transmit_port_settings()[0].port == new_transmit_port_settings[0].port
-        assert self.__transmit_port_settings()[0].enabled == True
-        assert self.__transmit_port_settings()[3].name == new_transmit_port_settings[3].name
-        assert self.__transmit_port_settings()[3].port == new_transmit_port_settings[3].port
-        assert self.__transmit_port_settings()[3].enabled == True
+        assert (
+            self.__transmit_port_settings()[0].name
+            == new_transmit_port_settings[0].name
+        )
+        assert (
+            self.__transmit_port_settings()[0].port
+            == new_transmit_port_settings[0].port
+        )
+        assert self.__transmit_port_settings()[0].enabled is True
+        assert (
+            self.__transmit_port_settings()[3].name
+            == new_transmit_port_settings[3].name
+        )
+        assert (
+            self.__transmit_port_settings()[3].port
+            == new_transmit_port_settings[3].port
+        )
+        assert self.__transmit_port_settings()[3].enabled is True
 
-        assert set(client._port for client in self.osc_duplicator.clients) == set([tps.port for tps in new_transmit_port_settings])
+        assert set(
+            client._port for client in self.osc_duplicator.clients
+        ) == set([tps.port for tps in new_transmit_port_settings])
 
     def test_save_settings(self):
         """
         正しくjsonファイルを書き出せるかテスト
         """
+
         def __random_name():
-            return "".join(random.choices(string.ascii_letters + string.digits, k=8))
+            return "".join(
+                random.choices(string.ascii_letters + string.digits, k=8)
+            )
 
         def __random_port():
             return random.randrange(start=0, stop=65535, step=1)
 
         # Arrange
         new_receive_port = 8001
-        new_transmit_port_settings = [TransmitPortSetting(__random_name(), __random_port(), True) for _ in range(4)]
+        new_transmit_port_settings = [
+            TransmitPortSetting(__random_name(), __random_port(), True)
+            for _ in range(4)
+        ]
 
         # Act
         SAVE_FILE_PATH = "./tests/test_save_Settings.json"
-        self.osc_duplicator.update_settings(new_receive_port, new_transmit_port_settings)
+        self.osc_duplicator.update_settings(
+            new_receive_port, new_transmit_port_settings
+        )
         self.osc_duplicator.save_settings(SAVE_FILE_PATH)
 
         self.osc_duplicator.load_settings(SAVE_FILE_PATH)
@@ -131,12 +167,24 @@ class TestDuplicator:
         # Assert
         assert self.osc_duplicator.receive_port == 8001
 
-        assert self.__transmit_port_settings()[0].name == new_transmit_port_settings[0].name
-        assert self.__transmit_port_settings()[0].port == new_transmit_port_settings[0].port
-        assert self.__transmit_port_settings()[0].enabled == True
-        assert self.__transmit_port_settings()[3].name == new_transmit_port_settings[3].name
-        assert self.__transmit_port_settings()[3].port == new_transmit_port_settings[3].port
-        assert self.__transmit_port_settings()[3].enabled == True
+        assert (
+            self.__transmit_port_settings()[0].name
+            == new_transmit_port_settings[0].name
+        )
+        assert (
+            self.__transmit_port_settings()[0].port
+            == new_transmit_port_settings[0].port
+        )
+        assert self.__transmit_port_settings()[0].enabled is True
+        assert (
+            self.__transmit_port_settings()[3].name
+            == new_transmit_port_settings[3].name
+        )
+        assert (
+            self.__transmit_port_settings()[3].port
+            == new_transmit_port_settings[3].port
+        )
+        assert self.__transmit_port_settings()[3].enabled is True
 
     # def test_transmit_msg(self):
     #     """
