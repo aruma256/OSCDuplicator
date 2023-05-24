bf5327506781f4352e2a15473e71d8f0efbe0c2d
DESKTOP2022\h195m <h195mic@gmail.com>
2023-05-24 16:59:04 +0900

    [新設計#6]に基づくバックエンド部分の作成

--- oscduplicator/duplicator.py
+++ oscduplicator/duplicator.py
@@ -1,250 +1,70 @@
-import json
-import queue
-import socket
-from dataclasses import asdict
-from threading import Thread
+from pathlib import Path
 
-from pythonosc.dispatcher import Dispatcher
-from pythonosc.osc_server import ThreadingOSCUDPServer
-from pythonosc.udp_client import SimpleUDPClient
-from oscduplicator.setting import TransmitPortSetting
+from oscduplicator.receiver import OSCReceiver
+from oscduplicator.settings import Settings
+from oscduplicator.transmitter import OSCTransmitter
 
 
-class OSCDuplicator:
+class Duplicator:
     """
-    OSC信号を受信するためのサーバーと、再送信のための設定を保持する
+    OSCDuplicatorのバックエンドを統括するクラス
 
     Attributes
-    ---------
-    server: osc_server.ThreadingOSCUDPServer
-        OSC信号を受信するためのサーバー
-    receive_port: int
-        OSC信号を受信するためのポート、デフォルト9001
-    transmit_port_settings: list[TransmitPortSetting]
-        転送先ポートの設定を保持するリスト
-    clients: list[udp_client.SimpleUDPClient]
-        OSC信号を再送信するための、clientのリスト
-    q: queue.Queue
-        OSC信号の受信順・送信順を保証するためのキュー
+    settings: Settings
+        送信・受信に関する設定を保持するクラスのインスタンスオブジェクト
+    receiver: OSCReceiver
+        OSC messageを受信するクラスのインスタンスオブジェクト
+    transmitter: OSCTransmitter
+        OSC messageを各ポートへ転送するクラスのインスタンスオブジェクト
     """
 
+    FILE_PATH = Path(".\oscduplicator\settings.json")
+
     def __init__(self) -> None:
-        self.__receive_port: int | None = None
-        self.__transmit_port_settings: list[TransmitPortSetting] = []
-        self.__server: ThreadingOSCUDPServer | None = None
-        self.clients: list[SimpleUDPClient] = []
-        self.__q = queue.Queue()
+        self.settings = Settings(Duplicator.FILE_PATH)
+        self.receiver = OSCReceiver(self.settings.receive_port)
+        self.transmitter = OSCTransmitter(self.receiver.q)
 
-    @property
-    def receive_port(self):
-        return self.__receive_port
-
-    @receive_port.setter
-    def receive_port(self, value: int):
+    def start_duplicate(self, receive_port, transmit_port_settings):
         """
-        __receiver_portのsetter
-        値のチェックをする
+        On start button pushed
 
-        Parameters
-        ---------
-        value: int
-            新しい受信ポート番号
-        """
-        # check value
-        if not isinstance(value, int):
-            raise TypeError(
-                "Expected instance of type 'int' for attribute 'port'"
-            )
-        if not (0 <= value <= 65535):
-            raise ValueError(
-                "Invalid 'port' value. Expected a number between 0 and 65535"
-            )
-        self.__receive_port = value
-
-    def start_server(self) -> None:
-        """
-        OSCサーバーを初期化し、起動する
-        """
-        port = self.receive_port if self.receive_port is not None else 9001
-        dpt = Dispatcher()
-
-        # dpt.map("/*", print)
-        dpt.map("/*", self.__queue_osc)
-
-        self.__server = ThreadingOSCUDPServer(
-            ("0.0.0.0", port), dpt
-        )
-        self.__server.serve_forever()
-
-    def stop_server(self) -> None:
-        """
-        OSCサーバーを終了する
-        """
-        self.__server.shutdown()
-
-    def __queue_osc(self, address: str, *args: list) -> None:
-        """
-        受信したOSC信号をdictとして、キューに追加する
-
-        Parameters
-        ---------
-        address: str
-            受信・再送信アドレス
-        args: list
-            osc信号
-        """
-        d = {"address": address, "args": args}
-        self.__q.put(d)
-
-    def transmit_msg(self, clients: list[SimpleUDPClient]) -> None:
-        """
-        osc信号を転送する
-
-        Parameters
-        ---------
-        transmit_clients: list[udp_client.SimpleUDPClient]
-            OSC信号を再送信するための、clientのリスト
-        q: queue.Queue
-            キュー
-            {"address": address, "args": args}
-        """
-
-        if not clients:
-            return
-
-        while True:
-            d = self.__q.get()
-            address: str = d["address"]
-            args: list = d["args"]
-
-            threads = [
-                Thread(
-                    target=client.send_message,
-                    args=(
-                        address,
-                        args[0],
-                    ),
-                )
-                for client in clients
-            ]
-
-            for thread in threads:
-                thread.start()
-
-            for thread in threads:
-                thread.join()
-
-            self.__q.task_done()
-
-    def load_settings(self, file_path: str) -> None:
-        """
-        jsonファイルを読み込み、
-        self.__transmit_port_settingsと
-        self.clients
-        を更新する
-        起動時に呼び出される
-
-        Parameters
-        ---------
-        file_path: str
-            設定ファイルのパス
-        """
-        self.receive_port, self.__transmit_port_settings = self.__load_json(
-            file_path
-        )
-        self.__update_clients(self.__transmit_port_settings)
-
-    def __load_json(self, file_path: str):
-        """
-        jsonファイルから設定データを呼び出す
-
-        Parameters
-        ---------
-        file_path: str
-            設定ファイルのパス
-
-        Returns
+        Attribute
         ---------
         receive_port: int
-            受信ポート
-        transmit_list: list[TransmitPortSettings]
-            分配ポートのセッティング(dataclass)のリスト
+            OSCReceiverのポート
+        transmit_port_settings: list[TransmitPortSettings]
+            OSCTransmitterのための設定
         """
+        self.__update_settings(receive_port, transmit_port_settings)
 
-        with open(file_path, "r", encoding="UTF-8") as f:
-            json_dic = json.load(f)
+        self.receiver.receive_port = self.settings.receive_port
+        self.receiver.init_server()
 
-        receive_port = json_dic["receive"]["port"]
+        self.transmitter.transmit_ports = self.settings.get_transmit_ports()
+        self.transmitter.init_clients(self.transmitter.transmit_ports)
 
-        transmit_port_settings: list[TransmitPortSetting] = [
-            TransmitPortSetting(**i) for i in json_dic["transmit"]
-        ]
+        self.receiver.start_receiver()
+        self.transmitter.start_transmitter()
 
-        return receive_port, transmit_port_settings
-
-    def __update_clients(
-        self, transmit_port_settings: list[TransmitPortSetting]
-    ) -> None:
+    def stop_duplicate(self):
         """
-        self.__clientsを更新する
-        load_settings()と
-        update_settings()で呼び出される
-
-        Parameters
+        On stop button pushed
         """
+        self.receiver.stop_receiver()
+        self.transmitter.stop_transmitter()
 
-        def __client(port: int) -> SimpleUDPClient:
-            hostname = socket.gethostname()
-            ip = socket.gethostbyname(hostname)
-            str_ip = str(ip)
-            return SimpleUDPClient(str_ip, port)
-
-        port_l: list[int] = []
-        for tps in transmit_port_settings:
-            if tps.enabled:
-                port_l.append(tps.port)
-
-        port_l = list(set(port_l))
-
-        self.clients = [__client(i) for i in port_l]
-
-    def update_settings(
-        self,
-        receive_port: int,
-        transmit_port_settings: list[TransmitPortSetting],
-    ) -> None:
+    def __update_settings(self, receive_port, transmit_port_settings):
         """
-        GUIでの入力内容から、
-        self.receive_portと
-        self.__transmit_port_settingsと
-        self.clients を更新する
-        OSCDuplicator内のstart_server()の前とsave_settings()の前に呼び出す
-
         Parameters
         ---------
         receive_port: int
-        transmit_port_settings: list[TransmitPortSetting]
-            app側では入力内容からlist[TransmitPortSetting]を作成して引数として設定する
-            なんか二度手間なような気がする
+            OSCReceiverのポート
+        transmit_port_settings: list[TransmitPortSettings]
+            OSCTransmitterのための設定
         """
-        self.receive_port = receive_port
-        self.__transmit_port_settings = transmit_port_settings
-        self.__update_clients(self.__transmit_port_settings)
+        self.settings.update_receive_port_setting(receive_port)
+        self.settings.update_transmit_port_settings(transmit_port_settings)
 
-    def save_settings(self, file_path: str) -> None:
-        """
-        メンバ変数をjsonファイルにセーブする
-        終了時に呼び出す
-        """
-
-        transmit_settings: list[dict] = [
-            asdict(d) for d in self.__transmit_port_settings
-        ]
-
-        save_data: dict = {
-            "receive": {"port": self.receive_port},
-            "transmit": transmit_settings,
-        }
-
-        with open(file_path, "w", encoding="UTF-8") as f:
-            json.dump(save_data, f, indent=4, ensure_ascii=False)
+    def save_settings(self):
+        self.settings.save_json(Duplicator.FILE_PATH)
---
+++ oscduplicator/transmitter.py
@@ -0,0 +1,71 @@
+from threading import Thread
+from queue import Queue
+import socket
+
+from pythonosc.udp_client import SimpleUDPClient
+
+from receiver import OSCMessage
+
+
+class OSCTransmitter:
+    """
+    QueueからOSC messageを取り出し、各ポートへ転送する
+
+    Attributes
+    ---------
+    transmit_ports: list[int]
+        OSC信号を再送信するための、portのリスト
+    clients: list[SimpleUDPClient]
+        OSC信号を再送信するための、clientのリスト
+    q: Queue
+        OSC信号の受信順・送信順を保証するためのキュー
+    is_shutdown: bool
+        transmitterを停止するためのフラグ
+
+    """
+
+    def __init__(self, q: Queue) -> None:
+        self.transmit_ports: list[int] = []
+        self.__q: Queue = q
+        self.clients: list[SimpleUDPClient] = self.init_clients(
+            self.transmit_ports
+        )
+        self.is_shutdown = False
+
+    def init_clients(self):
+        """
+        OSCクライエントを初期化
+        """
+
+        def __client(port: int) -> SimpleUDPClient:
+            hostname = socket.gethostname()
+            ip = socket.gethostbyname(hostname)
+            str_ip = str(ip)
+            return SimpleUDPClient(str_ip, port)
+
+        return [__client(i) for i in self.transmit_ports]
+
+    def start_transmitter(self):
+        """
+        OSCTransmitterを起動
+        """
+        th = Thread(target=self.transmit_forever)
+        th.start()
+
+    def transmit_forever(self):
+        self.is_shutdown = False
+
+        while not self.is_shutdown:
+            self.transmit_message(self.__q, self.clients)
+
+    def transmit_message(self, q: Queue, clients: list[SimpleUDPClient]):
+        if not clients:
+            osc_message: OSCMessage = q.get()
+
+            for client in clients:
+                client.send_message(osc_message.address, osc_message.message)
+
+            q.task_done()
+
+    def stop_transmitter(self):
+        self.is_shutdown = True
