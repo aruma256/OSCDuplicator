56fadf05a22072382fcffe9b7c6e1d369c463430
aruma256 <22253623+aruma256@users.noreply.github.com>
2023-05-28 14:14:18 +0900

    CIを通す

--- tests/test_setting.py
+++ tests/test_setting.py
@@ -1,47 +1,50 @@
 import json
+from unittest.mock import Mock, mock_open
+
 import pytest
-from pathlib import Path
-from unittest.mock import mock_open
+
 from oscduplicator.transmit_port_setting import TransmitPortSetting
 from oscduplicator.settings import Settings
 
 
 @pytest.fixture
-def mock_save_data():
+def save_data():
     return {
         "receive": {
-            "port": 9001
+            "port": 9001,
         },
         "transmit": [
             {
                 "name": "test0_t",
                 "port": 9001,
-                "enabled": True
+                "enabled": True,
             },
             {
                 "name": "test1_t",
                 "port": 9002,
-                "enabled": True
-            }
-        ]
+                "enabled": True,
+            },
+        ],
     }
 
-def test_load_json(mocker, mock_save_data):
-    mock_file = mock_open(read_data=json.dumps(mock_save_data))
-    mocker.patch('pathlib.Path.open', return_value=mock_file(), create=True)
 
-    settings = Settings(Path("mock_path.json"))
+def test_load_json(save_data):
+    mock_path = Mock()
+    mock_path.open = mock_open(read_data=json.dumps(save_data))
 
-    assert settings.receive_port == mock_save_data["receive"]["port"]
-    assert settings.transmit_port_settings == [TransmitPortSetting(**i) for i in mock_save_data["transmit"]]
+    settings = Settings(mock_path)
+
+    assert settings.receive_port == save_data["receive"]["port"]
+    assert settings.transmit_port_settings == [TransmitPortSetting(**i) for i in save_data["transmit"]] # noqa
 
 
-def test_get_transmit_ports(mocker, mock_save_data):
-    transmit_port_settings = [TransmitPortSetting(**i) for i in mock_save_data["transmit"]]
+def test_get_transmit_ports(save_data):
+    mock_path = Mock()
+    mock_path.open = mock_open(read_data=json.dumps(save_data))
 
-    mocker.patch.object(Settings, 'load_json', return_value=(mock_save_data["receive"]["port"], transmit_port_settings))
-    settings = Settings(Path("mock_path.json"))
+    settings = Settings(mock_path)
 
-    expected_port_numbers = [i["port"] for i in mock_save_data["transmit"]]
+    settings.transmit_port_settings = [TransmitPortSetting(**i) for i in save_data["transmit"]] # noqa
+
+    expected_port_numbers = [i["port"] for i in save_data["transmit"]]
     assert settings.get_transmit_ports() == expected_port_numbers
-
