259278a6e05db0b9f153922344d659174838bfc3
DESKTOP2022\h195m <h195mic@gmail.com>
2023-05-28 11:21:23 +0900

    settings.pyに関する単体テストの作成

--- tests/test_setting.py
+++ tests/test_setting.py
@@ -1,38 +1,47 @@
+import json
 import pytest
+from pathlib import Path
+from unittest.mock import mock_open
+from oscduplicator.transmit_port_setting import TransmitPortSetting
+from oscduplicator.settings import Settings
 
-from oscduplicator.setting import TransmitPortSetting
+
+@pytest.fixture
+def mock_save_data():
+    return {
+        "receive": {
+            "port": 9001
+        },
+        "transmit": [
+            {
+                "name": "test0_t",
+                "port": 9001,
+                "enabled": True
+            },
+            {
+                "name": "test1_t",
+                "port": 9002,
+                "enabled": True
+            }
+        ]
+    }
+
+def test_load_json(mocker, mock_save_data):
+    mock_file = mock_open(read_data=json.dumps(mock_save_data))
+    mocker.patch('pathlib.Path.open', return_value=mock_file(), create=True)
+
+    settings = Settings(Path("mock_path.json"))
+
+    assert settings.receive_port == mock_save_data["receive"]["port"]
+    assert settings.transmit_port_settings == [TransmitPortSetting(**i) for i in mock_save_data["transmit"]]
 
 
-class TestTransmitPortSetting:
-    def test_init_valid(self):
-        tps = TransmitPortSetting("Test", 8080, True)
-        assert tps.name == "Test"
-        assert tps.port == 8080
-        assert tps.enabled is True
+def test_get_transmit_ports(mocker, mock_save_data):
+    transmit_port_settings = [TransmitPortSetting(**i) for i in mock_save_data["transmit"]]
 
-    def test_init_transforms_name(self):
-        tps = TransmitPortSetting(1234, 8080, True)
-        assert tps.name == "1234"
+    mocker.patch.object(Settings, 'load_json', return_value=(mock_save_data["receive"]["port"], transmit_port_settings))
+    settings = Settings(Path("mock_path.json"))
 
-    def test_invalid_port_raises_exception(self):
-        with pytest.raises(ValueError):
-            TransmitPortSetting("Test", 70000, True)
+    expected_port_numbers = [i["port"] for i in mock_save_data["transmit"]]
+    assert settings.get_transmit_ports() == expected_port_numbers
 
-    def test_invalid_enabled_raises_exception(self):
-        with pytest.raises(TypeError):
-            TransmitPortSetting("Test", 8080, "True")
-
-    def test_set_invalid_port_raises_exception(self):
-        tps = TransmitPortSetting("Test", 8080, True)
-        with pytest.raises(ValueError):
-            tps.port = 70000
-
-    def test_set_invalid_enabled_raises_exception(self):
-        tps = TransmitPortSetting("Test", 8080, True)
-        with pytest.raises(TypeError):
-            tps.enabled = "True"
-
-    def test_set_transforms_name(self):
-        tps = TransmitPortSetting("Test", 8080, True)
-        tps.name = 1234
-        assert tps.name == "1234"
