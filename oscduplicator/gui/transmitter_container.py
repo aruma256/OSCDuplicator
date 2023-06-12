13ddbd1e2fd4fe1c2a8c1d933b297202c9074b7f
DESKTOP2022\h195m <h195mic@gmail.com>
2023-06-12 13:54:41 +0900

    add button, delete buttonで設定を追加・削除

--- oscduplicator/gui/transmitter_container.py
+++ oscduplicator/gui/transmitter_container.py
@@ -3,6 +3,7 @@ from functools import partial
 
 from oscduplicator.duplicator import Duplicator
 
+
 class TransmitterContainer(ft.UserControl):
     """
     GUIのうちTransmitterの設定部分
@@ -19,6 +20,8 @@ class TransmitterContainer(ft.UserControl):
         self.duplicator = duplicator
 
     def build(self):
+        self.settings_row = self.data_table_rows()
+
         return ft.Container(
             width=600,
             bgcolor=ft.colors.LIGHT_BLUE_50,
@@ -33,7 +36,7 @@ class TransmitterContainer(ft.UserControl):
                             ft.DataColumn(ft.Text("有効化")),
                             ft.DataColumn(ft.Text("")),
                         ],
-                        rows=self.data_table_rows(),
+                        rows=self.settings_row,
                     ),
                     ft.ElevatedButton(
                         text="追加",
@@ -50,63 +53,89 @@ class TransmitterContainer(ft.UserControl):
         self.transmitter_settingsから,
         DataTableに表示するためのDataRowのリストを作成する
         """
-
-        def create_row(port="", name="", enabled=False):
-            port_text = ft.Text(value=port)
-            name_text = ft.Text(value=name)
-
-            return ft.DataRow(
-                cells=[
-                    ft.DataCell(port_text),
-                    ft.DataCell(name_text),
-                    ft.DataCell(ft.Checkbox(value=enabled)),
-                    ft.DataCell(ft.IconButton(icon=ft.icons.DELETE_FOREVER)),
-                ]
-            )
-
         if not self.duplicator.settings.transmit_port_settings:
-            return [create_row()]
+            return [self.create_row()]
         else:
             return [
-                create_row(setting[0], setting[1], setting[2])
+                self.create_row(setting[0], setting[1], setting[2])
                 for setting in self.duplicator.settings.transmit_port_settings
             ]
 
+    def create_row(self, port="", name="", enabled=False):
+        port_text_field = ft.Text(value=port)
+        name_text_field = ft.Text(value=name)
+
+        return ft.DataRow(
+            cells=[
+                ft.DataCell(port_text_field),
+                ft.DataCell(name_text_field),
+                ft.DataCell(ft.Checkbox(value=enabled)),
+                ft.DataCell(
+                    ft.IconButton(
+                        icon=ft.icons.DELETE_FOREVER,
+                        on_click=partial(
+                            self.on_delete_clicked, port_text_field
+                        ),
+                    )
+                ),
+            ]
+        )
+
     def show_transmitter_add_dialog(self, e):
+        port_text_field = ft.TextField(label="port", width=100)
+        name_text_field = ft.TextField(label="name", width=100)
+
         e.page.dialog = ft.AlertDialog(
             content=ft.Column(
                 controls=[
                     ft.Text("転送設定"),
                     ft.Row(
                         controls=[
-                            ft.TextField(label="port", width=100),
-                            ft.TextField(label="name", width=100),
+                            port_text_field,
+                            name_text_field,
                         ]
                     ),
                 ],
                 height=100,
             ),
             actions=[
-                ft.TextButton("追加", on_click=self.close_dialog),
-                ft.TextButton("キャンセル", on_click=self.close_dialog),
+                ft.TextButton(
+                    "追加",
+                    on_click=partial(
+                        self.on_add_confirm, port_text_field, name_text_field
+                    ),
+                ),
+                ft.TextButton("キャンセル", on_click=self.on_cancel),
             ],
             actions_alignment=ft.MainAxisAlignment.END,
         )
         e.page.dialog.open = True
         e.page.update()
 
-    def on_edit_confirm(self, e, port_text, name_text):
-        pass
+    def on_add_confirm(self, port_text_field, name_text_field, e):
+        port = int(port_text_field.value)
+        name = name_text_field.value
 
-    def on_edit_cancel(self, e):
-        pass
+        row = self.create_row(port, name, False)
+        self.settings_row.append(row)
+        self.duplicator.settings.transmit_port_settings.append(
+            [port, name, False]
+        )
+        e.page.dialog.open = False
+        e.page.update()
+        self.update()
 
-    def on_add_confirm(self, e):
-        pass
+    def on_delete_clicked(self, port_text_field, _):
+        port = port_text_field.value
 
-    def on_add_cancel(self, e):
-        pass
+        self.duplicator.transmitter.remove_destination_port(port)
+        self.duplicator.settings.remove_transmit_port_setting(port)
 
-    def close_dialog(self, e):
+        self.settings_row.clear()
+        self.settings_row.append(*self.data_table_rows())
+
+        self.update()
+
+    def on_cancel(self, e):
         e.page.dialog.open = False
         e.page.update()
